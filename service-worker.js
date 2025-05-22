cc8c41f015640ced77e75186141c4750496f6a66
sala233sa <saebra581@gmail.com>
2025-05-22 14:36:23 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'tetris-offline-v3';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Instalación: Cachear recursos críticos
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Estrategia: Cache-First
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
+
+// Limpieza de cachés viejas
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => 
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    ).then(() => self.clients.claim())
+  );
+});
